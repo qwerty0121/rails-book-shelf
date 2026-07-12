7005b46f6cdafeb262aa87f1edf04a54398eb1bd
qwerty0121 <46868301+qwerty0121@users.noreply.github.com>
2026-07-12 23:46:54 +0900

    feat: Bookモデルを追加 (#10)
    タイトル・著者名・読了状態・メモを持つBookモデルとマイグレーションを追加する。

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_12_142036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "books", force: :cascade do |t|
+    t.string "author_name"
+    t.datetime "created_at", null: false
+    t.text "memo"
+    t.boolean "read"
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
 end
