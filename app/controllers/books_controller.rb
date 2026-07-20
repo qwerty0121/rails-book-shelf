3d4f4aa08afda07c79adb9a88191a3863daa9433
qwerty0121 <sh1ng0718o@gmail.com>
2026-07-20 07:42:52 +0000

    feat: BooksControllerのcreateアクションを実装
    createアクションとルーティングを追加し、蔵書を保存できるようにする。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,5 +9,21 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def book_params
+      params.expect(book: [ :title, :author_name, :read, :memo ])
+    end
 end
